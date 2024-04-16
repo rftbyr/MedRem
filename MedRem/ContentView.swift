b139daaa73bfd3ebd7729ea9c550242a3d31de99
rftbyr <rftbyr@icloud.com>
2024-04-16 23:14:57 +0300

    Initial Commit

---
+++ MedRem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MedRem
+//
+//  Created by Rıfat Bayar on 16.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedRem/MedRemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedRemApp.swift
+//  MedRem
+//
+//  Created by Rıfat Bayar on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MedRemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
